732fed9207220f9bba4bfa349a83977880d2be75
eliquious <max.franks@synapse-wireless.com>
2015-07-28 23:29:41 -0500

    Increase parser coverage

--- skl/parser_test.go
+++ skl/parser_test.go
@@ -4,11 +4,29 @@ import (
 	"reflect"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/suite"
 )
 
-// Ensure the parser can parse strings into Statement ASTs.
-func TestParser_ParseStatement(t *testing.T) {
+// In order for 'go test' to run this suite, we need to create
+// a normal test function and pass our suite to suite.Run
+func TestParserTestSuite(t *testing.T) {
+	suite.Run(t, new(ParserTestSuite))
+}
 
+// ParserTestSuite executes all the parser tests
+type ParserTestSuite struct {
+	suite.Suite
+}
+
+func (suite *ParserTestSuite) SetupTest() {
+}
+
+func (suite *ParserTestSuite) TearDownTest() {
+}
+
+// Ensure the parser can parse strings into Statement ASTs.
+func (suite *ParserTestSuite) TestStatements() {
 	var tests = []struct {
 		skip bool
 		s    string
@@ -21,22 +39,23 @@ func TestParser_ParseStatement(t *testing.T) {
 		},
 
 		// Errors
+		{s: `a bad statement.`, err: `found a, expected USE at line 1, char 1`},
+		{s: `USE `, err: `found EOF, expected namespace at line 1, char 6`},
 		{s: `USE acme.example.`, err: `found EOF, expected identifier at line 1, char 18`},
 		{s: `USE acme.example. `, err: `found WS, expected identifier at line 1, char 18`},
 	}
 
 	for i, tt := range tests {
 		if tt.skip {
-			t.Logf("skipping test of '%s'", tt.s)
+			suite.T().Logf("skipping test of '%s'", tt.s)
 			continue
 		}
 		stmt, err := NewParser(strings.NewReader(tt.s)).ParseStatement()
 
 		if !reflect.DeepEqual(tt.err, errstring(err)) {
-			t.Errorf("%d. %q: error mismatch:\n  exp=%s\n  got=%s\n\n", i, tt.s, tt.err, err)
+			suite.T().Errorf("%d. %q: error mismatch:\n  exp=%s\n  got=%s\n\n", i, tt.s, tt.err, err)
 		} else if tt.err == "" && !reflect.DeepEqual(tt.stmt, stmt) {
-			// t.Logf("\nexp=%s\ngot=%s\n", mustMarshalJSON(tt.stmt), mustMarshalJSON(stmt))
-			t.Errorf("%d. %q\n\nstmt mismatch:\n\nexp=%#v\n\ngot=%#v\n\n", i, tt.s, tt.stmt, stmt)
+			suite.T().Errorf("%d. %q\n\nstmt mismatch:\n\nexp=%#v\n\ngot=%#v\n\n", i, tt.s, tt.stmt, stmt)
 		}
 	}
 }
