5e4d0658c21dcc81f8685e5eaf2a0984490714f5
eliquious <max.franks@synapse-wireless.com>
2015-09-09 00:51:19 -0500

    Refactor SSH server to use new Config and fix shutdown bugs

--- ssh/server.go
+++ ssh/server.go
@@ -1,18 +1,12 @@
 package ssh
 
 import (
-	"crypto/rand"
-	"crypto/x509"
 	"fmt"
 	"net"
 	"time"
 
-	log "github.com/mgutz/logxi/v1"
-	"github.com/spf13/viper"
-
-	"github.com/subsilent/kappa/datamodel"
-	"github.com/subsilent/zbase32"
 	"golang.org/x/crypto/ssh"
+	tomb "gopkg.in/tomb.v2"
 )
 
 const (
@@ -23,134 +17,147 @@ const (
 	CsrfTokenLength     = 40
 )
 
-func NewSSHServer(logger log.Logger, sys datamodel.System, privateKey ssh.Signer, roots *x509.CertPool) (server SSHServer, err error) {
+func NewSSHServer(cfg *Config) (server SSHServer, err error) {
 
-	// Get user store
-	users, err := sys.Users()
-
-	// Create server config
-	sshConfig := &ssh.ServerConfig{
-		NoClientAuth: false,
-		PublicKeyCallback: func(conn ssh.ConnMetadata, key ssh.PublicKey) (perm *ssh.Permissions, err error) {
-
-			// Get user if exists, otherwise return error
-			user, err := users.Get(conn.User())
-			if err != nil {
-				return
-			}
-
-			// Check keyring for public key
-			if keyring := user.KeyRing(); !keyring.Contains(key.Marshal()) {
-				err = fmt.Errorf("invalid public key")
-				return
-			}
-
-			// Add pubkey and username to permissions
-			perm = &ssh.Permissions{
-				Extensions: map[string]string{
-					"pubkey":   string(key.Marshal()),
-					"username": conn.User(),
-				},
-			}
-			return
-		},
-		AuthLogCallback: func(conn ssh.ConnMetadata, method string, err error) {
-			if err != nil {
-				logger.Info("Login attempt", "user", conn.User(), "method", method, "error", err.Error())
-			} else {
-				logger.Info("Successful login", "user", conn.User(), "method", method)
-			}
-		},
+	// Create ssh config for server
+	sshConfig, e := cfg.SSHConfig()
+	if e != nil {
+		err = e
+		return
 	}
-	sshConfig.AddHostKey(privateKey)
+	cfg.sshConfig = sshConfig
 
-	// Get ssh bind addr
-	bind := viper.GetString("SSHListen")
-	if bind == "" {
-		err = fmt.Errorf("Empty SSH bind address")
+	// Validate the ssh bind addr
+	if cfg.Bind == "" {
+		err = fmt.Errorf("ssh server: Empty SSH bind address")
 		return
 	}
 
 	// Open SSH socket
-	logger.Info("Starting SSH server", "addr", bind)
-	sshAddr, err := net.ResolveTCPAddr("tcp", bind)
-	if err != nil {
-		err = fmt.Errorf("Invalid tcp address")
+	sshAddr, e := net.ResolveTCPAddr("tcp", cfg.Bind)
+	if e != nil {
+		err = fmt.Errorf("ssh server: Invalid tcp address")
 		return
 	}
 
 	// Create listener
-	listener, err := net.ListenTCP("tcp", sshAddr)
-	if err != nil {
+	listener, e := net.ListenTCP("tcp", sshAddr)
+	if e != nil {
+		err = e
 		return
 	}
-
-	server.logger = logger
-	server.sshConfig = sshConfig
 	server.listener = listener
-	server.system = sys
 	return
 }
 
 type SSHServer struct {
-	logger    log.Logger
-	sshConfig *ssh.ServerConfig
-	listener  *net.TCPListener
-	system    datamodel.System
-	done      chan bool
+	config   *Config
+	listener *net.TCPListener
+	t        tomb.Tomb
 }
 
-func (s *SSHServer) Run(logger log.Logger, closer chan<- bool) {
-	logger.Info("Starting SSH server", "addr", viper.GetString("SSHListen"))
-	s.done = make(chan bool)
+func (s *SSHServer) Start() {
+	s.config.Logger.Info("Starting SSH server", "addr", s.config.Bind)
+	s.t.Go(s.listen)
+}
 
-	// Start server
-	go func(l log.Logger, sock *net.TCPListener, config *ssh.ServerConfig, c <-chan bool, complete chan<- bool) {
-		defer sock.Close()
-		for {
+func (s *SSHServer) Stop() error {
+	s.t.Kill(nil)
+	s.config.Logger.Info("Shutting down SSH server...")
+	return s.t.Wait()
+}
 
-			// Accepts will only block for 1s
-			sock.SetDeadline(time.Now().Add(time.Second))
+func (s *SSHServer) listen() error {
+	defer s.listener.Close()
 
-			select {
+	// Create tomb for connection goroutines
+	var t tomb.Tomb
 
-			// Stop server on channel receive
-			case <-c:
-				l.Info("Stopping SSH server")
-				complete <- true
-				return
-			default:
+	for {
 
-				// Accept new connection
-				tcpConn, err := sock.Accept()
-				if err != nil {
-					if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
-						// l.Debug("Connection timeout...")
-					} else {
-						l.Warn("Connection failed", "error", err)
-					}
-					continue
+		// Accepts will only block for 1s
+		s.listener.SetDeadline(time.Now().Add(s.config.Deadline))
+
+		select {
+
+		// Stop server on channel receive
+		case <-s.t.Dying():
+			t.Kill(nil)
+			t.Wait()
+			return nil
+		default:
+
+			// Accept new connection
+			tcpConn, err := s.listener.Accept()
+			if err != nil {
+				if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+					s.config.Logger.Trace("Connection timeout...")
+				} else {
+					s.config.Logger.Warn("Connection failed", "error", err)
 				}
-
-				// Handle connection
-				l.Debug("Successful SSH connection")
-				go handleTCPConnection(l, tcpConn, config, s.system)
+				continue
 			}
+
+			// Handle connection
+			s.config.Logger.Debug("Successful TCP connection: %s", tcpConn.RemoteAddr())
+			t.Go(func() error {
+				return s.handleTCPConnection(t, tcpConn)
+			})
 		}
-	}(logger, s.listener, s.sshConfig, s.done, closer)
+	}
+	return nil
 }
 
-func (s *SSHServer) Wait() {
-	s.done <- true
-}
+func (s *SSHServer) handleTCPConnection(parentTomb tomb.Tomb, conn net.Conn) error {
 
-func generateToken(length int) (token string, err error) {
-	bytes := make([]byte, length)
-	if _, err := rand.Read(bytes); err != nil {
-		return "", err
+	// Convert to SSH connection
+	sshConn, channels, requests, err := ssh.NewServerConn(conn, s.config.sshConfig)
+	if err != nil {
+		s.config.Logger.Warn("SSH handshake failed: %s", conn.RemoteAddr())
+		return err
 	}
 
-	// stringify token
-	token, err = zbase32.EncodeAll(bytes)
-	return
+	// Close connection on exit
+	s.config.Logger.Debug("Handshake successful")
+	defer sshConn.Conn.Close()
+
+	// Discard requests
+	go ssh.DiscardRequests(requests)
+
+	// Create new tomb stone
+	var t tomb.Tomb
+
+	for {
+		select {
+		case ch := <-channels:
+			chType := ch.ChannelType()
+
+			// Determine if channel is acceptable (has a registered handler)
+			handler, ok := s.config.Handler(chType)
+			if !ok {
+				s.config.Logger.Info("UnknownChannelType", "type", chType)
+				ch.Reject(ssh.UnknownChannelType, chType)
+				break
+			}
+
+			// Accept channel
+			channel, requests, err := ch.Accept()
+			if err != nil {
+				s.config.Logger.Warn("Error creating channel")
+				continue
+			}
+
+			t.Go(func() error {
+				return handler.Handle(t, sshConn, channel, requests)
+			})
+		case <-parentTomb.Dying():
+			t.Kill(nil)
+			if err := t.Wait(); err != nil {
+				s.config.Logger.Warn("ssh handler error: %s", err)
+			}
+			sshConn.Close()
+			return sshConn.Wait()
+		}
+	}
+	return nil
 }
